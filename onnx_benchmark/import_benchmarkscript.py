8d3c312fd87a3dfac08c0ef7fe17d77592b2130b
Vandana Kannan <vandana268@gmail.com>
2018-11-27 15:55:48 -0800

    Fix ONNX instrumentation

--- onnx_benchmark/import_benchmarkscript.py
+++ onnx_benchmark/import_benchmarkscript.py
@@ -4,6 +4,7 @@ import glob
 import time
 import numpy as np
 
+
 def get_model_input(model_dir):
     import onnx
     from onnx import numpy_helper
@@ -26,7 +27,8 @@ def get_model_input(model_dir):
 
     return model_inputs
 
-def profile_model(model_path, test_data, context):   
+
+def profile_model(model_path, test_data, context):
     import mxnet as mx
 
     sym, arg_params, aux_params = mx.contrib.onnx.import_model(model_path)
@@ -37,8 +39,11 @@ def profile_model(model_path, test_data, context):
     inference_time = 0
     for data_idx, _ in enumerate(test_data):
         data_shapes = []
+        data_forward = []
         for idx, input_name in enumerate(data_names):
-            data_shapes.append((input_name, test_data[idx+data_idx].shape))
+            val = test_data[idx + data_idx]
+            data_shapes.append((input_name, val.shape))
+            data_forward.append(mx.nd.array(val))
 
         # create a module
         mod = mx.mod.Module(symbol=sym, data_names=data_names, context=ctx, label_names=None)
@@ -50,33 +55,28 @@ def profile_model(model_path, test_data, context):
         else:
             mod.set_params(arg_params=arg_params, aux_params=aux_params)
 
-        data_forward = []
-        for idx, input_name in enumerate(data_names):
-            val = test_data[idx+data_idx]
-            data_forward.append(mx.nd.array(val))
-
         start = time.time()
         mod.forward(mx.io.DataBatch(data_forward))
-        total_time =  (time.time() - start)*1000
-        #total_time = "{:.9f}".format(total_time)
+        _ = mod.get_outputs()[0].asnumpy()
+        total_time = (time.time() - start) * 1000
+        # total_time = "{:.9f}".format(total_time)
         # log_data.update({test_data_name: total_time})
         inference_time += total_time
-    
-    avg_inference_time = inference_time/len(test_data)
+
+    avg_inference_time = inference_time / len(test_data)
     return avg_inference_time
 
+
 if __name__ == '__main__':
-       from sys import argv
-       ctx = str(argv[1])
-       for directory in os.listdir("./models"):
-           model_dir = os.path.join("./models", directory)
-           if os.path.isdir(model_dir):
-               model_path = os.path.join(model_dir, "model.onnx")
-               test_data = get_model_input(model_dir)
+    from sys import argv
 
-               profile_data = profile_model(model_path, test_data, ctx)
+    ctx = str(argv[1])
+    for directory in os.listdir("./models"):
+        model_dir = os.path.join("./models", directory)
+        if os.path.isdir(model_dir):
+            model_path = os.path.join(model_dir, "model.onnx")
+            test_data = get_model_input(model_dir)
 
-               avg_inference_time = profile_model(model_path, test_data, ctx)
+            avg_infer_time = profile_model(model_path, test_data, ctx)
 
-               print('Average_inference_time_{}_{}: {:.9f}'.format(directory, ctx, avg_inference_time))
-
+            print('Average_inference_time_{}_{}: {:.9f}'.format(directory, ctx, avg_infer_time))
