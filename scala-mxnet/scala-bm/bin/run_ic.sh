5518db3de489d01f0a5ce27ae83a92b26d8da7d9
Piyush Ghai <ghai.8@osu.edu>
2019-01-23 15:18:53 -0800

    Added profiles type to pom file and removed it from the shell script

--- scala-mxnet/scala-bm/bin/run_ic.sh
+++ scala-mxnet/scala-bm/bin/run_ic.sh
@@ -10,23 +10,14 @@ then
     hw_type=gpu
 fi
 
-platform=linux-x86_64
-
-if [[ $OSTYPE = [darwin]* ]]
-then
-    platform=osx-x86_64
-fi
-
-
 SCALA_VERSION_PROFILE=2.11
 MXNET_VERSION="[1.5.0-SNAPSHOT,)"
 
 CURR_DIR=$(cd $(dirname $0)/../; pwd)
 
 echo $CURR_DIR
-echo $platform-$hw_type
 
-./mvnw clean install dependency:copy-dependencies package -Dmxnet.profile=$platform-$hw_type -Dmxnet.scalaprofile=$SCALA_VERSION_PROFILE -Dmxnet.version=$MXNET_VERSION
+./mvnw clean install dependency:copy-dependencies package -Dmxnet.hw_type=$hw_type -Dmxnet.scalaprofile=$SCALA_VERSION_PROFILE -Dmxnet.version=$MXNET_VERSION
 
 CURR_DIR=$(cd $(dirname $0)/../; pwd)
 
