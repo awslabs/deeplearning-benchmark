169c539ac57ac2f9f729e484cc946230cd8a2f86
Piyush Ghai <ghai.8@osu.edu>
2019-01-30 14:37:10 -0800

    Changing Resnet50 to Resnet18 for Scala (#51)

---
+++ scala-mxnet/scala-bm/bin/get_resnet18_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+
+data_path=/tmp/resnet-18/
+
+image_path=$data_path/images/
+
+if [ ! -d "$data_path" ]; then
+  mkdir -p "$data_path"
+fi
+
+if [ ! -d "$image_path" ]; then
+  mkdir -p "$image_path"
+fi
+
+if [ ! -f "$data_path" ]; then
+  wget https://s3.us-east-2.amazonaws.com/scala-infer-models/resnet-18/resnet-18-symbol.json -P $data_path
+  wget https://s3.us-east-2.amazonaws.com/scala-infer-models/resnet-18/resnet-18-0000.params -P $data_path
+  wget https://s3.us-east-2.amazonaws.com/scala-infer-models/resnet-18/synset.txt -P $data_path
+  wget https://s3.us-east-2.amazonaws.com/mxnet-scala/scala-example-ci/resnet152/kitten.jpg -P $image_path
+fi
