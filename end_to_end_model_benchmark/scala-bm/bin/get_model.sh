3be4aa32afb0f4b4a64a4a30852a13a847964969
stu1130 <gstu1130@gmail.com>
2019-02-15 14:31:08 -0800

    upload the env setup script

---
+++ end_to_end_model_benchmark/scala-bm/bin/get_model.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+CURR_DIR=$(cd $(dirname $0)/../; pwd)
+
+if [[ $1 = e2e ]]
+then
+    model_url="https://s3.us-east-2.amazonaws.com/mxnet-public/end_to_end_models"
+    model_name="resnet18_v1_end_to_end"
+else
+    model_url="../models/not_end_to_end_model/resnet18_v1"
+    model_name="resnet18_v1"
+fi
+
+mkdir models
+cd models
+wget "$model_url/$model_name-symbol.json"
+wget "$model_url/$model_name-0000.params"
