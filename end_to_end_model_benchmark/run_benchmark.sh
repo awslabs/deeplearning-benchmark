8f55a08bfd95568374f3f01706503821f10aac39
stu1130 <gstu1130@gmail.com>
2019-03-04 10:35:11 -0800

    change the warmup to 5

--- end_to_end_model_benchmark/run_benchmark.sh
+++ end_to_end_model_benchmark/run_benchmark.sh
@@ -41,7 +41,7 @@ output_single=$(java -Xmx8G  -cp $CLASSPATH mxnet.EndToEndModelWoPreprocessing \
 --model-path-prefix $model_path \
 --num-runs $4 \
 --batchsize 1 \
---warm-up 15 \
+--warm-up 5 \
 $end_to_end \
 $use_gpu)
 
