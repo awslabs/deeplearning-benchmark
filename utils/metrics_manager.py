ce3f9ed4a542ec192a54c6da83f88027d2003a0d
Chaitanya Prakash Bapat <chai.bapat@gmail.com>
2019-09-16 14:07:09 -0700

    fix benchmark issues with py3 (#74)
    * fix python3 decode
    
    * fix #75
    
    * list

--- utils/metrics_manager.py
+++ utils/metrics_manager.py
@@ -55,7 +55,7 @@ class BenchmarkResultManager(object):
         if not os.path.isfile(log_file_location):
             raise Exception("log file was missing!")
         with open(log_file_location, 'rb') as f:
-            self.log_file = f.read()
+            self.log_file = f.read().decode('utf-8')
         assert isinstance(metric_patterns, list), "metric_patterns is expected to be a list."
         assert isinstance(metric_names, list), "metric_names is expected to be a list."
         assert isinstance(metric_compute_methods, list), "metric_compute_methods is expected to be a list."
@@ -91,7 +91,7 @@ class BenchmarkResultManager(object):
             metric = map(self.__get_float_number, metric)
             metric_result = BenchmarkMetricComputeMethod.compute(
                 metric_compute_method=self.metric_compute_methods[i],
-                metric=metric
+                metric=[metric]
             )
             self.metric_map[name] = metric_result
         self.metric_map['uptime_in_seconds'] = self.uptime()
